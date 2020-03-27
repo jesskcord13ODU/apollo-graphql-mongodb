3b71cd00b2d429484388014da0d4d774dcc54237
Chanceler Shaffer <shafferchance@gmail.com>
2020-03-26 23:38:55 -0400

    Revert "Added the ability to mutate specific specification entry. Added a new endpoint in Go server to handle this. Also need to create HTTP enforce on our endpoints asap, proably..."

---
+++ src/components/Login.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import {LoginButton} from './LoginModal';
+import {NewUserButton} from './NewUserModal';
+import logo from '../img/museLogo.png';
+import '../App.css';
+import { Card, CardHeader, CardBody, Row, Col } from 'reactstrap';
+
+export const Login = () => {
+    return (
+        <Card className="login shadow">
+            <CardHeader className={"Login-padding"}>
+                <img src={logo} className="App-logo" alt="logo" />
+                <h5 className="font-weight-bold">
+                    Mission Engineering &amp; Integration
+                </h5>
+            </CardHeader>
+            <CardBody className="text-center">
+                <Row className="d-flex justify-content-center">
+                    <Col xs={"8"}>
+                        <LoginButton />
+                        <NewUserButton />
+                    </Col>
+                </Row>
+                <span className="text-center Login-padding Login-body">
+                    Or <a href="#">Login using a CAC / PIV card</a>
+                </span>
+            </CardBody>
+        </Card>
+    );
+}
